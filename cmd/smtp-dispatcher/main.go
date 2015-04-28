ce1fac2401ced66580b5e7777c23c845f479052a
John Weldon <johnweldon4@gmail.com>
2015-04-28 10:14:39 -0700

    make windows service

--- cmd/smtp-dispatcher/main.go
+++ cmd/smtp-dispatcher/main.go
@@ -2,79 +2,100 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
-	"os/signal"
-	"time"
+	"path/filepath"
+	"strings"
 
-	"gopkg.in/tomb.v2"
+	"code.google.com/p/winsvc/svc"
 
 	"github.com/golang/glog"
 
 	config "github.com/johnweldon/mqd/config"
-	"github.com/johnweldon/mqd/dispatcher"
-	"github.com/johnweldon/mqd/mailer"
+)
+
+const (
+	svcName        = "MailQueueDispatcher"
+	svcFriendly    = "Mail Queue Dispatcher"
+	svcDescription = "Mail Queue Dispatcher watches a mailqueue folder and uses configured smtp connections " +
+		"to relay the mail on.  If there are problems with delivery, or if the sender does not have a configured " +
+		"smtp connection, the mail file will be moved to a badmail folder for manual review"
 )
 
 var (
 	settingsfile     = ".smtp-dispatcher.settings"
 	generateSettings = false
-	settings         config.Settings
 )
 
 func init() {
-	flag.StringVar(&settingsfile, "settingsfile", settingsfile, "json encoded settings file")
-	flag.StringVar(&settingsfile, "sf", settingsfile, "json encoded settings file (short version)")
-
 	flag.BoolVar(&generateSettings, "generate", generateSettings, "generate settings file")
 	flag.BoolVar(&generateSettings, "g", generateSettings, "generate settings file (short version)")
-
-	flag.Set("log_dir", ".")
 }
 
 func main() {
+	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		glog.Fatalf("couldn't find program folder %q\n", err)
+		os.Exit(1)
+	}
+	settingsfile = filepath.Join(pwd, settingsfile)
+	flag.Set("log_dir", pwd)
 	flag.Parse()
+
+	interactive, err := svc.IsAnInteractiveSession()
+	if err != nil {
+		glog.Fatalf("couldn't tell if we're in an interactive session: %v", err)
+		os.Exit(2)
+	}
+
+	if !interactive {
+		runService(svcName, false)
+		return
+	}
 
 	if generateSettings {
 		generate()
 		return
 	}
 
-	s, err := config.ReadSettings(settingsfile)
-	if err != nil {
-		glog.Fatalf("couldn't read settings %q\n", err)
-		os.Exit(2)
+	if len(flag.Args()) < 1 {
+		usage("no command specified")
 	}
 
-	settings = s
-	runLoop()
-	os.Exit(0)
+	cmd := strings.ToLower(flag.Arg(0))
+	switch cmd {
+	case "debug":
+		runService(svcName, true)
+		return
+	case "install":
+		err = installService(svcName, svcFriendly, svcDescription)
+	case "uninstall", "remove":
+		err = removeService(svcName)
+	case "start":
+		err = startService(svcName)
+	case "stop":
+		err = controlService(svcName, svc.Stop, svc.Stopped)
+	case "pause":
+		err = controlService(svcName, svc.Pause, svc.Paused)
+	case "continue":
+		err = controlService(svcName, svc.Continue, svc.Running)
+	default:
+		usage(fmt.Sprintf("invalid command %s", cmd))
+	}
+	if err != nil {
+		glog.Fatalf("failed to %s %s: %v", cmd, svcName, err)
+		os.Exit(3)
+	}
+	return
 }
 
-func runLoop() {
-	q := dispatcher.NewPickupFolderQueue(settings.MailQueue, settings.BadMail)
-	m := mailer.NewMailer(settings)
-
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, os.Kill)
-
-	var t tomb.Tomb
-	t.Go(func() error {
-		q.Process(m.ConvertAndSend)
-		for {
-			select {
-			case s := <-c:
-				glog.Infof("got signal %v", s)
-				glog.Flush()
-				return nil
-			case <-t.Dying():
-				return nil
-			case <-time.After(time.Duration(settings.Interval) * time.Second):
-				q.Process(m.ConvertAndSend)
-				glog.Flush()
-			}
-		}
-	})
-	t.Wait()
+func usage(message string) {
+	fmt.Fprintf(os.Stderr, "\n%s\n\n"+
+		"usage: %s <command>\n"+
+		"    where <command> is one of\n"+
+		"    install, remove, debug, start, stop, pause, or continue.\n\n",
+		message, os.Args[0])
+	os.Exit(8)
 }
 
 func generate() {
@@ -90,7 +111,7 @@ func generate() {
 
 	if err := config.WriteSettings(settingsfile, settings); err != nil {
 		glog.Fatalf("couldn't write settings %q\n", err)
-		os.Exit(3)
+		os.Exit(9)
 	}
 	os.Exit(0)
 }
---
+++ cmd/smtp-dispatcher/service.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"code.google.com/p/winsvc/debug"
+	"code.google.com/p/winsvc/eventlog"
+	"code.google.com/p/winsvc/mgr"
+	"code.google.com/p/winsvc/svc"
+	"github.com/golang/glog"
+
+	config "github.com/johnweldon/mqd/config"
+	"github.com/johnweldon/mqd/dispatcher"
+	"github.com/johnweldon/mqd/mailer"
+)
+
+var elog debug.Log
+
+type service struct {
+	settingsfile string
+}
+
+func (s *service) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
+	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown | svc.AcceptPauseAndContinue
+	changes <- svc.Status{State: svc.StartPending}
+	settings := s.readSettings()
+	tick := time.NewTicker(time.Duration(settings.Interval) * time.Second)
+	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+loop:
+	for {
+		select {
+		case <-tick.C:
+			s.runDispatch()
+		case c := <-r:
+			switch c.Cmd {
+			case svc.Interrogate:
+				changes <- c.CurrentStatus
+				time.Sleep(100 * time.Millisecond)
+				changes <- c.CurrentStatus
+			case svc.Stop, svc.Shutdown:
+				break loop
+			case svc.Pause:
+				tick.Stop()
+				changes <- svc.Status{State: svc.Paused, Accepts: cmdsAccepted}
+			case svc.Continue:
+				settings := s.readSettings()
+				tick = time.NewTicker(time.Duration(settings.Interval) * time.Second)
+				changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+			default:
+				elog.Error(1, fmt.Sprintf("unexpected control request #%d", c))
+			}
+		}
+	}
+	changes <- svc.Status{State: svc.StopPending}
+	return
+}
+
+func (s *service) runDispatch() {
+	settings := s.readSettings()
+	q := dispatcher.NewPickupFolderQueue(settings.MailQueue, settings.BadMail)
+	m := mailer.NewMailer(settings)
+	q.Process(m.ConvertAndSend)
+	glog.Flush()
+}
+
+func (s *service) readSettings() config.Settings {
+	settings, err := config.ReadSettings(settingsfile)
+	if err != nil {
+		glog.Errorf("couldn't read settings: %v", err)
+		settings = config.NewSettings("", "")
+	}
+	return settings
+}
+
+func runService(name string, isDebug bool) {
+	var err error
+	if isDebug {
+		elog = debug.New(name)
+	} else {
+		elog, err = eventlog.Open(name)
+		if err != nil {
+			glog.Fatalf("couldn't open eventlog: %v", err)
+			return
+		}
+	}
+	defer elog.Close()
+
+	elog.Info(1, fmt.Sprintf("starting %s service", name))
+	run := svc.Run
+	if isDebug {
+		run = debug.Run
+	}
+	err = run(name, &service{settingsfile: settingsfile})
+	if err != nil {
+		elog.Error(1, fmt.Sprintf("%s service failed: %v", name, err))
+		return
+	}
+	elog.Info(1, fmt.Sprintf("%s service stopped", name))
+}
+
+func startService(name string) error {
+	m, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+	defer m.Disconnect()
+
+	s, err := m.OpenService(name)
+	if err != nil {
+		return fmt.Errorf("could not access service %s: %v", name, err)
+	}
+	defer s.Close()
+
+	err = s.Start([]string{})
+	if err != nil {
+		return fmt.Errorf("could not start service %s: %v", name, err)
+	}
+
+	return nil
+}
+
+func controlService(name string, c svc.Cmd, to svc.State) error {
+	m, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+	defer m.Disconnect()
+
+	s, err := m.OpenService(name)
+	if err != nil {
+		return fmt.Errorf("could not access service %s: %v", name, err)
+	}
+	defer s.Close()
+
+	status, err := s.Control(c)
+	if err != nil {
+		return fmt.Errorf("could not send control=%d to %s: %v", c, name, err)
+	}
+
+	timeout := time.Now().Add(10 * time.Second)
+	for status.State != to {
+		if timeout.Before(time.Now()) {
+			return fmt.Errorf("timeout waiting for the service %s to go to state=%d", name, to)
+		}
+		time.Sleep(300 * time.Millisecond)
+		status, err = s.Query()
+		if err != nil {
+			return fmt.Errorf("could not retrieve service %s status: %v", name, err)
+		}
+	}
+	return nil
+}
+
+func installService(name, friendly, desc string) error {
+	exepath, err := exePath()
+	if err != nil {
+		return err
+	}
+	m, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+	defer m.Disconnect()
+
+	s, err := m.OpenService(name)
+	if err == nil {
+		s.Close()
+		return fmt.Errorf("service %s already exists", name)
+	}
+	cfg := mgr.Config{DisplayName: friendly, Description: desc, StartType: mgr.StartAutomatic}
+	s, err = m.CreateService(name, exepath, cfg)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	err = eventlog.InstallAsEventCreate(name, eventlog.Error|eventlog.Warning|eventlog.Info)
+	if err != nil {
+		s.Delete()
+		return fmt.Errorf("SetupEventLogSource() failed: %s", err)
+	}
+	return nil
+}
+
+func removeService(name string) error {
+	m, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+	defer m.Disconnect()
+
+	s, err := m.OpenService(name)
+	if err != nil {
+		return fmt.Errorf("service %s is not installed", name)
+	}
+	defer s.Close()
+
+	err = s.Delete()
+	if err != nil {
+		return err
+	}
+
+	err = eventlog.Remove(name)
+	if err != nil {
+		return fmt.Errorf("RemoveEventLogSource() failed: %s", err)
+	}
+	return nil
+}
+
+func exePath() (string, error) {
+	prog := os.Args[0]
+	p, err := filepath.Abs(prog)
+	if err != nil {
+		return "", err
+	}
+	fi, err := os.Stat(p)
+	if err == nil {
+		if !fi.Mode().IsDir() {
+			return p, nil
+		}
+		err = fmt.Errorf("%s is directory", p)
+	}
+	if filepath.Ext(p) == "" {
+		p += ".exe"
+		fi, err := os.Stat(p)
+		if err == nil {
+			if !fi.Mode().IsDir() {
+				return p, nil
+			}
+			err = fmt.Errorf("%s is directory", p)
+		}
+	}
+	return "", err
+}
