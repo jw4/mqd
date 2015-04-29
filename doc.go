c6f785b41ed83c87638f886b967f1f1f6229e22a
John Weldon <johnweldon4@gmail.com>
2015-04-29 11:14:15 -0700

    documentation

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Copyright 2015 John Weldon. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE.md file.
+
+/*
+Package dispatcher provides functionality to watch a mailqueue folder
+and periodically process messages that appear in that folder by sending
+them to configured smtp host destinations.
+*/
+package dispatcher
